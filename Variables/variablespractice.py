56f48fb00c24c9623be0ca40b03d915c5ab888ae
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-01-29 09:53:32 -0700

    python practive

---
+++ Variables/variablespractice.py
@@ -0,0 +1,27 @@
+# Lisa Rehm, Variables Practice Python
+
+name = "Lisa Rehm"
+
+small_number = "7"
+
+large_number = "256"
+
+breakfast = "air"
+
+favorite_color = "Red"
+
+school = "UCAS"
+
+year = "2025"
+
+eye_color = "blue"
+
+age = "15"
+
+favorite_subject = "chemistry"
+
+name = input("What is your name?")
+
+small_number = ("Give me a number between 1 and 10.")
+
+large_number = ("Now give me a number between 100 and 1000")
