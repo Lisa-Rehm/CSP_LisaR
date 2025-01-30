f9858be3835fb96c03d813b9508322214d4c4121
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-01-30 09:25:06 -0700

    changes to practice

--- Variables/variablespractice.py
+++ Variables/variablespractice.py
@@ -17,11 +17,3 @@ year = "2025"
 eye_color = "blue"
 
 age = "15"
-
-favorite_subject = "chemistry"
-
-name = input("What is your name?")
-
-small_number = ("Give me a number between 1 and 10.")
-
-large_number = ("Now give me a number between 100 and 1000")
