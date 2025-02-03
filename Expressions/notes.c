0595c228314da591fcc18b2a0e9702c5d4cf2d1e
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-02-03 09:53:10 -0700

    more expressions

--- Expressions/notes.c
+++ Expressions/notes.c
@@ -4,10 +4,16 @@
 // Floats float when we set the variable and %f when we print
 // Strings (words) char when we set the variable and %s when we print, %c gives us the first character
 int mynum;
+float percent;
 
 int main(void){
     printf("Type a number: \n");
     scanf("%d", &mynum);
-    printf("You inputed %d", mynum);
+    printf("You inputed %d", mynum;)
+    printf("Give me a percent as a decimal: \n");
+    scanf("%f", &percent);
+    printf("Your percent is %f\n", percent);3
     return 0;
 }
+
+//give it the data type then where to send it, the place you are sending it has to be premade
