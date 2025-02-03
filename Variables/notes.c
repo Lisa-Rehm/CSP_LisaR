496a4a28d9cc2450d4b1e68bf931cad2503ec30b
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-02-03 09:19:36 -0700

    notes c

---
+++ Variables/notes.c
@@ -0,0 +1,13 @@
+// Lisa Rehm, Variables Notes
+#include <stdio.h>
+
+char name[] = "Lisa";
+int num = 42;
+float pi = 3.14;
+
+int main(void){
+    printf("%s\n", name);
+    printf("%d\n", num);
+    printf("%f\n", pi);
+    return 0;
+}
