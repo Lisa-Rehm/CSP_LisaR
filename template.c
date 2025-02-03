bcc09c821e18b9a2a9d93b3516c59cdeec567e56
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-02-03 09:22:22 -0700

    template

--- template.c
+++ template.c
@@ -1,12 +1,7 @@
+//Lisa Rehm, Template
 #include <stdio.h>
 
-char name[] = "Lisa";
-int num = 42;
-float pi = 3.14;
-
 int main(void){
-    printf("%s\n", name);
-    printf("%d\n", num);
-    printf("%f\n", pi);
+    printf("Hello World");
     return 0;
 }
