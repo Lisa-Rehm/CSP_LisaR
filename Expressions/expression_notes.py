a8068bd99287cedfc7b97af2e75b7a330d665f23
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-02-04 09:27:11 -0700

    notes

--- Expressions/expression_notes.py
+++ Expressions/expression_notes.py
@@ -13,3 +13,19 @@ print(10.5%5) #modulo/modulus or MOD Gives the remainder
 
 print(3+7*2)
 print((3+7)*2)
+
+x = 2
+
+print(x)
+
+x += 1 
+print(x)
+
+#adding and setting it to be equal at the same time
+#this works for *,/,-,+,**,//, and %. If you can use it in an expression.
+
+y = 9
+
+y %= 2
+
+print(y)
--- Expressions/notes.c
+++ Expressions/notes.c
@@ -1,18 +1,33 @@
 //Lisa Rehm, Expressions Notes C
 #include <stdio.h>
+#include <math.h> //this allows us to use exponents
 // Integers int when we set the variable and %d when we print
 // Floats float when we set the variable and %f when we print
 // Strings (words) char when we set the variable and %s when we print, %c gives us the first character
+// We set expressions as variables
+// have division as float because you can get a decimal
 int mynum;
 float percent;
+int add = 4+6;
+int mul = 4*6;
+int sub = 4-6;
+float div = 6/4; //division gives a whole number in c like '//' in python
+int mod = 6%4;
+int ex = pow(5, 2);
 
 int main(void){
-    printf("Type a number: \n");
-    scanf("%d", &mynum);
-    printf("You inputed %d", mynum;)
-    printf("Give me a percent as a decimal: \n");
-    scanf("%f", &percent);
-    printf("Your percent is %f\n", percent);3
+    //printf("Type a number: \n");
+    //scanf("%d", &mynum);
+    //printf("You inputed %d\n", mynum);
+    //printf("Give me a percent as a decimal: \n");
+    //scanf("%f", &percent);
+    //printf("Your percent is %f\n", percent);
+    printf("%d\n", add);
+    printf("%d\n", mul);
+    printf("%d\n", sub);
+    printf("%f\n", div);
+    printf("%d\n", mod);
+    printf("%d\n", ex);
     return 0;
 }
 
