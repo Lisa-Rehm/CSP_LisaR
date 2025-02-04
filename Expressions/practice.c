a8ed9e7a0f5eed86cc3dacd80fe8d17b78c66d12
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-02-04 09:53:52 -0700

    practice

---
+++ Expressions/practice.c
@@ -0,0 +1,7 @@
+//Lisa Rehm, Template
+#include <stdio.h>
+
+int main(void){
+    printf("Hello World");
+    return 0;
+}
---
+++ Expressions/practice.py
@@ -0,0 +1,13 @@
+#Lisa Rehm, Expression Practice Python
+
+x = 7-24/8*4+6
+y = 18/3-7+2*5
+z = 6*4/12+72/8-9
+a = (17-6/2)+4*3
+b = (-2*(1*4-2/2))+(6+2-3)
+
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
