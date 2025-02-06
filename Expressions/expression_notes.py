77824d1eebeb4a1a58526eddc4661e075e5a5d23
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-02-06 09:12:26 -0700

    new stuff

--- Expressions/expression_notes.py
+++ Expressions/expression_notes.py
@@ -10,7 +10,6 @@ print(10.5/5)
 print(10.5**5) #exponents
 print(10.5//5) #rounded division: gives whole number
 print(10.5%5) #modulo/modulus or MOD Gives the remainder
-
 print(3+7*2)
 print((3+7)*2)
 
