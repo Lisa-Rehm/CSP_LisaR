84d5a4620a39cec7b7bca9ec1ed109b74b6486b4
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-02-11 19:05:35 +0000

    fin calc c final

--- Expressions/financial_calc.c
+++ Expressions/financial_calc.c
@@ -34,10 +34,10 @@ int main(void){
     per_groceries = (groceries / income) * 100;
     per_transportation = (transportation / income) * 100;
     per_spending = (spending / income) * 100;
-    printf("You spend %.2f%% of your income on rent.\n", per_rent);
-    printf("You spend %.2f%% of your income on utilities.\n", per_utilities);
-    printf("You spend %.2f%% of your income on groceries.\n", per_groceries);
-    printf("You spend %.2f%% of your inome on transportation.\n", per_transportation);
+    printf("You spend $%.2f on rent which is %.2f%% of your income.\n", rent, per_rent);
+    printf("You spend $%.2f on utilities which is %.2f%% of your income.\n", utilities, per_utilities);
+    printf("You spend $%.2f on groceries which is %.2f%% of your income.\n", groceries, per_groceries);
+    printf("You spend $%.2f on transportation which is %.2f%% of your inome.\n", transportation, per_transportation);
     printf("You have $%.2f dollars left to spend, which is %.2f%% of your income.\n", spending, per_spending);
     printf("We are going to put $%.2f in your savings.\n", savings);
     return 0;
