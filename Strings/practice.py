6bed2cc2d93cefc60b32d03b7c967b7f826f5bcb
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-02-11 19:16:20 +0000

    strings py finish

--- Strings/practice.py
+++ Strings/practice.py
@@ -1,9 +1,10 @@
 # Lisa Rehm, Strings Practice Python
 
-#welcome to the program
 print("Welcome to my program!")
-#ask for a name as input
-name = input("Please tell me your first name").strip #more
-#concatenate decorations to name
 
-#print new name
+name = input("Please tell me your first name.\n").strip().capitalize()
+
+decor_one = "(|*"
+decor_two = "*|)"
+
+print("Your name is", decor_one + name + decor_two)
