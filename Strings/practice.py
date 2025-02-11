5e7ba175d691d50c33f251c431c3f2e4d0ecfd96
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-02-11 09:53:10 -0700

    pythons strings practice

---
+++ Strings/practice.py
@@ -0,0 +1,9 @@
+# Lisa Rehm, Strings Practice Python
+
+#welcome to the program
+print("Welcome to my program!")
+#ask for a name as input
+name = input("Please tell me your first name").strip #more
+#concatenate decorations to name
+
+#print new name
