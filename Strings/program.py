18ea1bb57ff5770d9ec7c07a2aad44db42d48d3f
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-02-13 09:52:59 -0700

    silly sentences py start

--- Strings/program.py
+++ Strings/program.py
@@ -1,1 +1,10 @@
 # Lisa Rehm, Silly Sentences
+
+#Introduce program, ask for first variable
+num_one = input("Please give me a number (no more than three digits).")
+#Ask for second
+
+#Ask for third
+
+#sentence (print statement)
+print("%d million years ago, there was a %s named %s. He had %d siblings who all loved %s.\n %d hated %s and always felt left out.\n Then the asteroid struck and everything was fine.\n", num_one, dinosaur, name, num_two, gerund, name, gerund)
