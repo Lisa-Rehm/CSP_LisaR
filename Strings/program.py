ffcb7433f2b86e9f5485ce4531d05ac4cd971242
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-02-14 09:45:39 -0700

    sily sentences .py final?

--- Strings/program.py
+++ Strings/program.py
@@ -1,10 +1,17 @@
 # Lisa Rehm, Silly Sentences
 
-#Introduce program, ask for first variable
-num_one = input("Please give me a number (no more than three digits).")
-#Ask for second
+print("Welcome! This is a MadLib, have fun!\n")
 
-#Ask for third
+num_one = input("Please give me a number (no more than three digits).\n").strip()
 
-#sentence (print statement)
-print("%d million years ago, there was a %s named %s. He had %d siblings who all loved %s.\n %d hated %s and always felt left out.\n Then the asteroid struck and everything was fine.\n", num_one, dinosaur, name, num_two, gerund, name, gerund)
+dinosaur = input("Please give me a type of dinosaur (one word please).\n").strip().capitalize()
+
+name = input("Please give me a name (one word please).\n").strip().capitalize()
+
+num_two = input("Please give me another number (no more than two digits).\n").strip()
+
+gerund = input("Please give me a sport that ends in -ing (one word please).\n").strip().lower()
+
+print("Here is your story:")
+
+print(f"{num_one} million years ago, there was a {dinosaur} named {name} who had {num_two} siblings who all loved {gerund}.\n {name} hated {gerund} and always felt left out.\n Then the asteroid struck and everything was fine.\n")
