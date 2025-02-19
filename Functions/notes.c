baf822f2c29aa07d670e2830003c8fdfd39dc051
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-02-19 09:53:21 -0700

    notes

--- Functions/notes.c
+++ Functions/notes.c
@@ -1,16 +1,16 @@
 //Lisa Rehm, Functions notes
 #include <stdio.h>
 
-//int add(void){ //we use {} in C, everything in the function must exist between the {}
-//    printf("%d", 6+8)
-//    return 6+8;
-//}
+int add(void){ //we use {} in C, everything in the function must exist between the {}
+    printf("%d", 6+8);
+    return 6+8;
+}
 
-//int main(void){
-//    printf("Hello World");
-//    add();
-//    return 0;
-//}
+int main(void){
+    printf("Hello World");
+    add();
+    return 0;
+}
 // whatever data type set at beginning (int) has to match the data type in the return (0)
 // we have to define things above the main function
 
@@ -25,28 +25,24 @@
 //    etc
 //}
 
-//void add(int, numOne,int numTwo){
-//    printf("%d\n", numOne + numTwo)
+
+
+//Madness: don't return strings in C :)
+//void add(int numOne,int numTwo){
+//    printf("%d\n", numOne+numTwo);
 //}
 
-//int main (void)
-//    printf("Hello World\n");
-//    add();
+//const char* word(char type[10], int length){
+//    char answer[10];
+//    printf("Please give me a %s:\n", type);
+//    getStr(answer, sizeof(answer)-1);
+//    return answer;
+//}
+
+//int main(void){
+//    word("name");
+//    word("verb");
+//    word("place");
+//    printf("%s was %s until they somehow reached %s", word("name", 50), word("verb", 50), word("place", 50));
 //    return 0;
 //}
-
-
-char input(char type[20]){
-    char answer[50];
-    printf("Please give me a %s:\n", 
-    type);
-    scanf("%s", answer);
-    return answer;
-}
-
-int main(void){
-    input("name");
-    input("verb");
-    input("verb");
-    printf("%s was %s until they somehow reached %s", input("name"), input("verb"), input("verb"));
-}
---
+++ Functions/practice.py
@@ -0,0 +1,11 @@
+# Lisa Rehm, Update Hello World Python
+
+def user(word):
+    return input(f"Tell me the {word} name:\n")
+first = user("first")
+second = user("second")
+third = user("third")
+
+print(f"The names you chose were {first}, {second}, and {third}.")
+
+# Finish as input or throw five names at them
