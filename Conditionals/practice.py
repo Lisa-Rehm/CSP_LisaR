bcf2f425bc6db84bb366c65d0b34c4ef6d561839
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-02-26 09:18:00 -0700

    old enough python

--- Conditionals/practice.py
+++ Conditionals/practice.py
@@ -2,15 +2,16 @@
 
 age = int(input("How old are you? (Please give a number).\n"))
 
-if age >= 5:
-    print("You are old enough to go to school!")
-    if age >= 15:
-        print("You are old enough to get a learners permit!")
-        if age >= 16:
-            print("You are old enough to drive!")
-            if age >= 18:
-                print("You are old enough to vote!")
+if age >= 18:
+    print("You are old enought to vote!")
 else:
-    print("You are not old enough to do anything. Sorry.")
-
-# Make it so that only one statement prints
+    if age >= 16:
+        print("You are old enough to drive!")
+    else:
+        if age >= 15:
+            print("You are old enough to get a learners permit!")
+        else:
+            if age >= 5:
+                print("You are old enough to go to school!")
+            else:
+                print("You aren't old enough to do anything, sorry.")
