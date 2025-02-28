d12064d322115539305f11ee8df263f9cfe28187
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-02-28 09:52:40 -0700

    time c

--- Conditionals/time.c
+++ Conditionals/time.c
@@ -2,23 +2,27 @@
 #include <stdio.h>
 #include <time.h>
 
+
 int main(void){
-    // {time_t seconds;
-    // seconds = time(NULL);
-    // printf("Seconds since January 1, 1970 = %d\n", seconds);}
-
-
-    time_t rawtime;
-    struct tm * timeinfo;
-
-    time(&rawtime);
-    timeinfo = localtime(&rawtime);
-    printf("Current time and date is %s\n", asctime(timeinfo));
-
-    //current hour
     time_t now = time(NULL);
     struct tm * tm_struct = localtime(&now);
     int hour = tm_struct->tm_hour;
     printf("%d\n", hour);
+
+    if (hour <= 7){
+        printf("Good morning! Tis the best time of day!.\n");
+    }else if(hour <= 11){
+        printf("Good near end of waking period, you should start preparing for sleep now.\n");
+    }else if(hour == 12){
+        printf("Good day! You should probably fall asleep now before the sun gets too bright.\n");
+    }else if(hour <= 16){
+        printf("Good afternoon! Enjoy your sleep!\n");
+    }else if(hour <= 19){
+        printf("Good evening! I hope you are well rested!\n");
+    }else if(hour <= 23){
+        printf("Good night, Enjoy your day!\n");
+    }else{
+        printf("Go back to the non-24-hour planet from which you came. Or stay. I don't care.\n");
+    }
     return 0;
 }
