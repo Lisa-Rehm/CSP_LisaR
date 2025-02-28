663407fa8a7b5b6f33fd3aca27c3ea9f457204d4
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-02-28 19:18:46 +0000

    kappa

---
+++ Zeta chi/kappa.py
@@ -0,0 +1,22 @@
+# Lisa Rehm, Kappa project
+
+    ## Look for *executive decision* to see things that need to be thought out more
+
+# Welcome to the program...
+
+# Did you want this to be beginner, intermediate, or advanced?
+# If user input = beginner call beginner function... etc.
+
+# Do you want to start with grammer, vocabulary...etc?
+# If user input = grammer call grammer function...etc
+
+## Grammer function
+# *executive decision* Will I ask the user which piece they want to do first, or give it to them in order? What if they don't want to answer a certain thing yet? How can I give them an updated list of things to choose from that doesn't include things they've already done? Perhaps if input = null then print in user input statement. Perhaps it has to be in a certain order or I can have a recommended order.
+
+# Sentence structure
+    # SVO, SOV, VOS, VSO, etc. provide examples (would you like to see examples for any?) and advice on which is best (would you like advice for which one to choose?). This advice should include reasons for different ones, such as SOV being the most common but others potentialy providing different meanings, etc.
+# Word order
+
+
+
+## Vocab function
