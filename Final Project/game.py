1af5daaa0a642ed3a51d689215a8cb534824a6e8
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-03-19 15:48:12 +0000

    Co-authored-by: EveR249 <EveR249@users.noreply.github.com>

--- Final Project/game.py
+++ Final Project/game.py
@@ -1,1 +1,39 @@
 # Lisa Rehm and Eve Richardson, Final Project
+
+# a variables, 2 functions, a conditional, a loop
+
+# Lisa: Loop, Functions
+
+# Eve variable (setting the numbers on the board to usable variables) and conditional
+
+#   1 | 2 | 3
+#  -----------
+#   4 | 5 | 6
+#  -----------
+#   7 | 8 | 9
+
+one = "1"
+two = "2"
+three = "3"
+four = "4"
+five = "5"
+six = "6"
+seven = "7"
+eight = "8"
+nine = "9"
+
+
+# Find a way to make the computer print our board
+print(f" {one} | {two} | {three} ")
+print("-----------")
+print(f" {four} | {five} | {six} ")
+print("-----------")
+print(f" {seven} | {eight} | {nine} ")
+
+
+
+
+# create a list of our variables and have the computer draw a random one that doesn't include one already picked
+
+# For computers turn
+#if random number 
