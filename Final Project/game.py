337405a1b883f6f1e209a1f9c29eae46db27a301
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-03-20 15:53:14 +0000

    conditionals mostly
    Co-authored-by: EveR249 <EveR249@users.noreply.github.com>

--- Final Project/game.py
+++ Final Project/game.py
@@ -2,9 +2,9 @@
 
 # a variables, 2 functions, a conditional, a loop
 
-# Lisa: Loop, Functions
+# Lisa: Functions that ask for user input and another that gets tha random number
 
-# Eve variable (setting the numbers on the board to usable variables) and conditional
+# Eve variable (setting the numbers on the board to usable variables) and conditional that checks if someone won
 
 #   1 | 2 | 3
 #  -----------
@@ -14,14 +14,13 @@
 
 one = "1"
 two = "2"
-three = "3"
-four = "4"
+three = "X"
+four = "O"
 five = "5"
 six = "6"
 seven = "7"
 eight = "8"
 nine = "9"
-
 
 # Find a way to make the computer print our board
 print(f" {one} | {two} | {three} ")
@@ -31,9 +30,27 @@ print("-----------")
 print(f" {seven} | {eight} | {nine} ")
 
 
+name = input("What is your name?\n")
 
 
 # draw a random number that create a list of our variables and have the computer draw a random one that doesn't include one already picked 
 # For computers turner
 # if random number = variable = X or O variable
-# rand = random.randint(1,20)
+def user():
+    taken_x = input("Your turn! Pick a number between 1 and 9:\n")
+
+import random
+rand = random.randint(1,9)
+
+# if there is 3 in a row then _ wins if one=two=three etc. else call loop
+# else loop (has functions inside it to do user input and random computer number)
+
+if one==two==three=="O" or four==five==six=="O" or seven==eight==nine=="O" or one==four==seven=="O" or two==five==eight=="O" or three==six==nine=="O" or one==five==nine=="O" or three==five==seven=="O":
+    print("The winner is the computer!")
+elif one==two==three=="X" or four==five==six=="X" or seven==eight==nine=="X" or one==four==seven=="X" or two==five==eight=="X" or three==six==nine=="X" or one==five==nine=="X" or three==five==seven=="X":
+    print(f"Congratulations {name}, you won!")
+elif one=="O" or "X" and two=="O" or "X" and three=="O" or "X" and four=="O" or "X" and five=="O" or "X" and six=="O" or "X" and seven=="O" or "X" and eight=="O" or "X" and nine=="O" or "X":
+    print("Tie game!")
+else:
+    print("No winner yet, keep playing.")
+   
