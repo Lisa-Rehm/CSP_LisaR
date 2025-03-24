bf6085b2ade1d76776a8b7cd4cc1713564c9860d
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-03-24 15:51:57 +0000

    com and user turns
    Co-authored-by: EveR249 <EveR249@users.noreply.github.com>

--- Final Project/game.py
+++ Final Project/game.py
@@ -8,15 +8,8 @@
 
 # Blaine:
 
-one = "1"
-two = "2"
-three = "3"
-four = "4"
-five = "5"
-six = "6"
-seven = "7"
-eight = "8"
-nine = "9"
+
+(one, two, three, four, five, six, seven, eight, nine) = (1, 2, 3, 4, 5, 6, 7, 8, 9)
 
 import random
 
@@ -31,6 +24,13 @@ def board():
     print("-----------")
     print(f" {seven} | {eight} | {nine} ")
 
+#def board():  
+#    for i in range(1, 10, 3):  
+#        print(f" {board[i]} | {board[i+1]} | {board[i+2]} ")  
+#        if i < 7:  
+#            print("-----------")  
+
+
 board()
 
 name = input("What is your name?\n")
@@ -40,39 +40,110 @@ name = input("What is your name?\n")
 # For computers turner
 # if random number = variable = X or O variable
 def user_turn():
-    taken_x = input("Your turn! Pick a number between 1 and 9:\n")
-    if taken_x == "1":
-        one = "X"
+    taken_x = input(int("Your turn! Pick a number between 1 and 9:\n"))
+    if taken_x == 1:
+        one = "X"  
+    elif taken_x == 2:
+        two = "X"
         board()
-    elif taken_x == "2":
-        two ="X"
-    elif taken_x == "3":
+    elif taken_x == 3:
         three = "X"
-    elif taken_x == "4":
+        board()
+    elif taken_x == 4:
         four = "X"
-    elif taken_x == "5":
+        board()
+    elif taken_x == 5:
         five = "X"
-    elif taken_x == "6":
+        board()
+    elif taken_x == 6:
         six = "X"
-    elif taken_x == "7":
+        board()
+    elif taken_x == 7:
         seven = "X"
-    elif taken_x == "8":
+        board()
+    elif taken_x == 8:
         eight = "X"
-    elif taken_x == "9":
+        board()
+    elif taken_x == 9:
         nine = "X"
+        board()
     else:
-        print(":/")
-
+        print("Else")
+        board()
+        
 
         
 
 
 
 
-# def com_turn():
-#   rand = random.randint(1,9)
-# if one == rand
+#def com_turn():
+#  rand = random.randint(1,9)
+#if one == rand
 
+
+#def com_turn():
+#    rand = random.randint(1,9)
+#    if rand == 1:
+#        if 1 =="X":
+#            com_turn()
+#        else:
+#            "1" = "O"
+#            board()
+#    elif rand == 2:
+#        if 2 =="X":
+#            com_turn()
+#        else:
+#            "2" = "O"
+#            board()
+#    elif rand == 3:
+#        if 3 =="X":
+#            com_turn()
+#        else:
+#            "3" = "O"
+#            board()
+#    elif rand == 4:
+#        if 4 =="X":
+#            com_turn()
+#        else:
+#            "4" = "O"
+#            board()
+#    elif rand == 5:
+#        if 5 =="X":
+#            com_turn()
+#        else:
+#            "5" = "O"
+#            board()
+#    elif rand == 6:
+#        if 6 =="X":
+#            com_turn()
+#        else:
+#            "6" = "O"
+#            board()
+#    elif rand == 7:
+#        if 7 =="X":
+#            com_turn()
+#        else:
+#            "7" = "O"
+#            board()
+#    elif rand == 8:
+#        if 8 =="X":
+#            com_turn()
+#        else:
+#            "8" = "O"
+#            board()
+#    elif rand == 9:
+#        if 9 =="X":
+#            com_turn()
+#        elif 9 =="O":
+#            com_turn()
+#        else:
+#            "9" = "O"
+#            board()
+#    else:
+#        print("Tie Game!")
+
+#com_turn()
 
 if one==two==three=="O" or four==five==six=="O" or seven==eight==nine=="O" or one==four==seven=="O" or two==five==eight=="O" or three==six==nine=="O" or one==five==nine=="O" or three==five==seven=="O":
     print("The winner is the computer!")
---
+++ Final Project/test.py
@@ -0,0 +1,7 @@
+# Lisa Rehm and Eve Richardson, Test
+
+one = "1"
+print(one)
+
+one = "X"
+print(one)
