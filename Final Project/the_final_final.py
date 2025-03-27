5d4316014d416d18afdc84ce1a62e9c9eb1fd492
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-03-27 15:23:51 +0000

    final project finish
    Co-authored-by: EveR249 <EveR249@users.noreply.github.com>
    Co-authored-by: DigitalRage <DigitalRage@users.noreply.github.com>

--- Final Project/the_final_final.py
+++ Final Project/the_final_final.py
@@ -1,139 +1,93 @@
-# Lisa Rehm and Eve Richardson, The Final Final
-winVar = 0
+# Lisa Rehm, Eve Richardson, and Blaine Buckler; The Final Final
+winVar = 0 #Made for my loop to work, Blaine
 import random
 
-rand = random.randint(1,9)
-
-# Eve
-(zero, one, two, three, four, five, six, seven, eight, nine) = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-
-# Lisa
+#Eve
+(zero, one, two, three, four, five, six, seven, eight, nine) = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9) #Zero is not used to reduce confusion, Blaine
+#Lisa
 spaces = [zero, one, two, three, four, five, six, seven, eight, nine]
 
-# Eve
+#Eve
 def board():
-    print(f" {spaces[0]} | {spaces[1]} | {spaces[2]} ")
+    print(f" {spaces[1]} | {spaces[2]} | {spaces[3]} ")
     print("-----------")
-    print(f" {spaces[3]} | {spaces[4]} | {spaces[5]} ")
+    print(f" {spaces[4]} | {spaces[5]} | {spaces[6]} ")
     print("-----------")
-    print(f" {spaces[6]} | {spaces[7]} | {spaces[8]} ")
+    print(f" {spaces[7]} | {spaces[8]} | {spaces[9]} ")
     print(" ")
 
 board()
 
-# Lisa
-def user_turn(spaces):
-    taken_x = int(input("Pick a number between 1 and 9:\n"))
-    if taken_x == 1 and spaces[0] == 1:
-        spaces[0] = "X"
-        board()
-        winner()
-    elif taken_x == 2 and spaces[1] == 2:
-        spaces[1] = "X"
-        board()
-        winner()
-    elif taken_x == 3 and spaces[2] == 3:
-        spaces[2] = "X"
-        board()
-        winner()
-    elif taken_x == 4 and spaces[3] == 4:
-        spaces[3] = "X"
-        board()
-        winner()
-    elif taken_x == 5 and spaces[4] == 5:
-        spaces[4] = "X"
-        board()
-        winner()
-    elif taken_x == 6 and spaces[5] == 6:
-        spaces[5] = "X"
-        board()
-        winner()
-    elif taken_x == 7 and spaces[6] == 7:
-        spaces[6] = "X"
-        board()
-        winner()
-    elif taken_x == 8 and spaces[7] == 8:
-        spaces[7] = "X"
-        board()
-        winner()
-    elif taken_x == 9 and spaces[8] == 9:
-        spaces[8] = "X"
+#Lisa and Blaine
+def user_turn():
+    taken_x = int(input("Pick a number between 1 and 9:\n")) #Debugged code to detect number and put in "taken_x" rather than elif for each number. I also stoped the infanite "blue_turn" glitch, Blaine
+    if 1 <= taken_x <= 9 and spaces[taken_x] == taken_x:
+        spaces[taken_x] = "X"
         board()
         winner()
     else:
         print("Pick a different number.")
-        user_turn(spaces)
-    return
-
-# Eve
+        user_turn() 
+#Eve and Blaine
 def winner():
-    if spaces[0]==spaces[1]==spaces[2]=="O" or spaces[3]==spaces[4]==spaces[5]=="O" or spaces[6]==spaces[7]==spaces[8]=="O" or spaces[0]==spaces[3]==spaces[6]=="O" or spaces[1]==spaces[4]==spaces[7]=="O" or spaces[2]==spaces[5]==spaces[8]=="O" or spaces[0]==spaces[4]==spaces[8]=="O" or spaces[2]==spaces[4]==spaces[6]=="O":
+    global winVar
+    if (spaces[1] == spaces[2] == spaces[3] == "O" or #Made easier to understand by putting each condition on differnt lines, Blaine
+        spaces[4] == spaces[5] == spaces[6] == "O" or
+        spaces[7] == spaces[8] == spaces[9] == "O" or
+        spaces[1] == spaces[4] == spaces[7] == "O" or
+        spaces[2] == spaces[5] == spaces[8] == "O" or
+        spaces[3] == spaces[6] == spaces[9] == "O" or
+        spaces[1] == spaces[5] == spaces[9] == "O" or
+        spaces[3] == spaces[5] == spaces[7] == "O"):
         print("Blue wins!")
-        #Blaine
         winVar = 1
-        #end
-    elif spaces[0]==spaces[1]==spaces[2]=="X" or spaces[3]==spaces[4]==spaces[5]=="X" or spaces[6]==spaces[7]==spaces[8]=="X" or spaces[0]==spaces[3]==spaces[6]=="X" or spaces[1]==spaces[4]==spaces[7]=="X" or spaces[2]==spaces[5]==spaces[8]=="X" or spaces[0]==spaces[4]==spaces[8]=="X" or spaces[2]==spaces[4]==spaces[6]=="X":
+
+    elif (spaces[1] == spaces[2] == spaces[3] == "X" or
+          spaces[4] == spaces[5] == spaces[6] == "X" or
+          spaces[7] == spaces[8] == spaces[9] == "X" or
+          spaces[1] == spaces[4] == spaces[7] == "X" or
+          spaces[2] == spaces[5] == spaces[8] == "X" or
+          spaces[3] == spaces[6] == spaces[9] == "X" or
+          spaces[1] == spaces[5] == spaces[9] == "X" or
+          spaces[3] == spaces[5] == spaces[7] == "X"):
         name = input("What is your name?\n")
         print(f"Congratulations {name}, you won!")
-        #Blaine
         winVar = 1
-        #end
+
+    elif (spaces[1] in ["X", "O"] and
+    spaces[2] in ["X", "O"] and
+    spaces[3] in ["X", "O"] and
+    spaces[4] in ["X", "O"] and 
+    spaces[5] in ["X", "O"] and 
+    spaces[6] in ["X", "O"] and 
+    spaces[7] in ["X", "O"] and
+    spaces[8] in ["X", "O"] and 
+    spaces[9] in ["X", "O"]): 
+        print("Tie Game")
+        winVar = 1
+        
     else:
         print("No winner yet, keep playing.")
-        #Blaine
-        winVar = 0
-        #end
 
-# Lisa
-def blue_turn(rand):
-    if rand == 1 and spaces[0] == 1:
-        spaces[0] = "O"
-        board()
-        winner()
-    elif rand == 2 and spaces[1] == 2:
-        spaces[1] = "O"
-        board()
-        winner()
-    elif rand == 3 and spaces[2] == 3:
-        spaces[2] = "O"
-        board()
-        winner()
-    elif rand == 4 and spaces[3] == 4:
-        spaces[3] = "O"
-        board()
-        winner()
-    elif rand == 5 and spaces[4] == 5:
-        spaces[4] = "O"
-        board()
-        winner()
-    elif rand == 6 and spaces[5] == 6:
-        spaces[5] = "O"
-        board()
-        winner()
-    elif rand == 7 and spaces[6] == 7:
-        spaces[6] = "O"
-        board()
-        winner()
-    elif rand == 8 and spaces[7] == 8:
-        spaces[7] = "O"
-        board()
-        winner()
-    elif rand == 9 and spaces[8] == 9:
-        spaces[8] = "O"
-        board()
-        winner()
-    else:
-        print("Blue is dumb")
-    return
+#Lisa
+def blue_turn():
+    board()
+    valid_move = False #Clears the variable to make it reusable, Blaine
+    while not valid_move:
+        move = random.randint(1, 9)  
+        if spaces[move] == move:
+            spaces[move] = "O"
+            valid_move = True #Gives a new integer to put in, Blaine
+    board()
+    winner()
 
+#Blaine
+def main(): #Code is used to check if game is over, Blaine
+    while winVar == 0:
+        user_turn()
+        if winVar:
+            break
+        blue_turn()
 
-# Blaine
-while winVar == 0: 
-    if winVar == 0:
-        user_turn(spaces)
-        blue_turn(rand)
-    else:
-        winVar == 1
-        break
-    
-    
+main()
+
