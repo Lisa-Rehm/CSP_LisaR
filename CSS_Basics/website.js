392d85eaa38fde637f949b2ae45019406ed8975d
Lisa-Rehm <lisa.rehm@ucas-edu.net>
2025-04-24 01:51:44 +0000

    website final

---
+++ CSS_Basics/website.js
@@ -0,0 +1,16 @@
+function push(){
+    document.getElementById("brodmann").style.display = "block"
+}
+let images = ["https://www.simplypsychology.org/wp-content/uploads/brodmanns-areas-1536x864.jpeg", "https://www.simplypsychology.org/wp-content/uploads/Brodmann-areas-map-1536x1194.jpeg"]
+
+count = 0
+function change(){
+    document.getElementById("img").style.width = "400px"
+    document.getElementById("img").src = "https://media.istockphoto.com/id/149078834/photo/mount-timpanogos.jpg?s=612x612&w=0&k=20&c=lAyRPoZZ17xlIfoYFDrx0ZP5HaIwh4u1AItsqNSpwxk="
+    document.getElementById("img").src = images[count]
+    if(count === 1){
+        count = 0
+    }else{
+        count += 1
+    }
+}
